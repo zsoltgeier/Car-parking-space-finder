1313db0ffb9a635016d767111bbb2c7f1510c227
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-07-18 17:20:26 +0200

    grey scaled and blurred the feed

--- main.py
+++ main.py
@@ -26,12 +26,14 @@ while True:
 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-
     success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
     checkParkingSpace()
     
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (0, 0, 255), 2)
     
     cv2.imshow("Image", img)
+    cv2.imshow("ImageBlur", imgBlur)
     cv2.waitKey(10)
