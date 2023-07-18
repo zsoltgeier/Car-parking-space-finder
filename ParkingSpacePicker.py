ec3c10dedbdf0897d3c3b45591062b32d85cacbf
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-07-18 16:36:53 +0200

    made mouseClick function

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -3,7 +3,16 @@ import pickle
 
 img = cv2.imread('carParkImg.png')
 
+width , height = 107, 48
+posList = []
+
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+
 while True:
-    cv2.rectangle(img, (50, 192), (157, 240), (0, 0, 255), 2)
-    cv2.imshow("image", img)
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (0, 0, 255), 2)
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouseClick)
     cv2.waitKey(1)
