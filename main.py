47919cd4bcffe6869860bf92ef72814b0c398adc
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-07-18 17:05:40 +0200

    saved rectangle positions to video feed

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ import numpy as np
 # Video feed
 cap = cv2.VideoCapture('carPark.mp4')
 
+with open('CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+
+width , height = 107, 48
 while True:
 
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
     success, img = cap.read()
+    
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (0, 0, 255), 2)
+
+    
     cv2.imshow("Image", img)
     cv2.waitKey(10)
