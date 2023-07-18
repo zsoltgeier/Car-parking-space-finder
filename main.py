e963b93eff7d0b5d3437a7a9129544857b4845b7
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-07-18 17:31:54 +0200

    set the cropped images be the dilated ones

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ with open('CarParkPos', 'rb') as f:
 
 width , height = 107, 48
 
-def checkParkingSpace():
+def checkParkingSpace(imgPro):
     for pos in posList:
         x, y = pos
         
 
-        imgCrop = img[y : y + height, x : x + width]
+        imgCrop = imgPro[y : y + height, x : x + width]
         cv2.imshow(str(x * y), imgCrop)
 
         
@@ -29,11 +29,18 @@ while True:
     success, img = cap.read()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
-    checkParkingSpace()
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernel, 1)
+
+
+    checkParkingSpace(imgDilate)
     
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (0, 0, 255), 2)
     
     cv2.imshow("Image", img)
     cv2.imshow("ImageBlur", imgBlur)
+    cv2.imshow("ImageThreshold", imgMedian)
     cv2.waitKey(10)
