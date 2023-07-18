81be153843a78ab4037eda31c6930646fb837fc2
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-07-18 16:29:27 +0200

    fixed rectangle position

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -4,6 +4,6 @@ import pickle
 img = cv2.imread('carParkImg.png')
 
 while True:
-    cv2.rectangle(img, (100, 230), (200, 150), (0, 0, 255), 2)
+    cv2.rectangle(img, (50, 192), (157, 240), (0, 0, 255), 2)
     cv2.imshow("image", img)
     cv2.waitKey(1)
